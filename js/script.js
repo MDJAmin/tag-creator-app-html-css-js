8fe02735b94439f0a06eb1e662d810f480b43c5c
MDJAmin <mohammadamindf@gmail.com>
2024-10-01 15:28:26 +0330

    Feat : add function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 "use strict"
 
+function addToken() {
+    const input = document.getElementById('tokenInput');
+    const tokenValue = input.value.trim();
 
+    if (tokenValue) {
+        const tokenContainer = document.querySelector('.token-container');
+        const newToken = document.createElement('div');
+        newToken.className = 'token';
+        newToken.onclick = function() { removeToken(this) };
+        newToken.innerHTML = `${tokenValue} <span class="close">&times;</span>`;
+        tokenContainer.insertBefore(newToken, input);
+        input.value = '';
+    }
+}
 
 function removeToken(token) {
     token.remove();

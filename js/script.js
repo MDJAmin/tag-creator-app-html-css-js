0fce6ae1d1824bf4056da50d2a09497d8157ee04
MDJAmin <mohammadamindf@gmail.com>
2024-10-01 15:31:32 +0330

    Clean code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-"use strict"
+"use strict";
 
 function addToken() {
-    const input = document.getElementById('tokenInput');
-    const tokenValue = input.value.trim();
+  const input = document.getElementById("tokenInput");
+  const tokenValue = input.value.trim();
 
-    if (tokenValue) {
-        const tokenContainer = document.querySelector('.token-container');
-        const newToken = document.createElement('div');
-        newToken.className = 'token';
-        newToken.onclick = function() { removeToken(this) };
-        newToken.innerHTML = `${tokenValue} <span class="close">&times;</span>`;
-        tokenContainer.insertBefore(newToken, input);
-        input.value = '';
-    }
+  if (tokenValue) {
+    const tokenContainer = document.querySelector(".token-container");
+    const newToken = document.createElement("div");
+    newToken.className = "token";
+    newToken.onclick = function () {
+      removeToken(this);
+    };
+    newToken.innerHTML = `${tokenValue} <span class="close">&times;</span>`;
+    tokenContainer.insertBefore(newToken, input);
+    input.value = "";
+  }
 }
 
 function removeToken(token) {
-    token.remove();
+  token.remove();
 }
